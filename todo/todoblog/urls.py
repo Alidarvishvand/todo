76c24e41b5537584e941050bd8062f1a37093319
Ali-darvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2024-05-18 15:36:43 +0330

    postlist
    see post in postlist view and added pagaination in postlist.html
    
    get query in view for seen post in post
    
    change function view to template view class

--- todo/todoblog/urls.py
+++ todo/todoblog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import index,remove
+from .views import IndexTemplate,PostListView
+from .views import remove
 
 urlpatterns = [
-    path('',index,name='index'),
-    path('', TemplateView.as_view(template_name="index.html")),
+    # path('',index,name='index'),
+    path('', IndexTemplate.as_view(template_name="index.html"), name='index'),
+    path('postlist/',PostListView.as_view(template_name="blog/post_list.html"), name='post'),
     path('del/<str:item_id>',remove, name="del"),
 ]
--- todo/todoblog/views.py
+++ todo/todoblog/views.py
@@ -1,35 +1,44 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from django.http import HttpResponse
 
 # import todo form and models
 
 from .forms import TodoForm
 from .models import Post
 
-###############################################
+class IndexTemplate(TemplateView):
+    template_name = "index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "TODO LIST"
+        context["form"] = TodoForm()
+        return context
+    def post(self, request):
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
 
-
-def index(request):
-
-	# item_list = Post.objects.order_by("-date")
-	if request.method == "POST":
-		form = TodoForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('index')
-	form = TodoForm()
-
-	page = {
-		"forms": form,
-
-		"title": "TODO LIST",
-	}
-	return render(request, 'index.html', page)
-
-
-### function to remove item, it receive todo item_id as primary key from url ##
 def remove(request, item_id):
-	item = Post.objects.get(id=item_id)
-	item.delete()
-	messages.info(request, "item removed !!!")
-	return redirect('index')
+    item = Post.objects.get(id=item_id)
+    item.delete()
+    messages.info(request, "item removed !!!")
+    return redirect('index')
+
+
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    model = Post
+    context_object_name = "posts"
+    paginate_by = 1
+    
+    def get_queryset(self):
+        posts = Post.objects.filter()
+        return posts
+    
